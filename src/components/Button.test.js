a49793ce771c42b98043f268db8d2564a09b8e79
Bohdana Filimon <bohdana.filimon@ubermetrics-technologies.com>
2021-02-20 19:43:50 +0100

    Create Button component

---
+++ src/components/Button.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Button from './Button';
+
+describe('Button test', () => {
+it('renders', () => {
+    render(
+      <Button
+        text="New Game"
+        onClick={() => {}}
+      />
+    );
+
+    expect(screen.getByRole('button')).toHaveTextContent('New Game');
+  });
+
+  it('calls handleClick on click', () => {
+    const handleClick = jest.fn();
+
+    render(
+      <Button
+        text="New Game"
+        onClick={handleClick}
+      />
+    );
+    fireEvent.click(screen.getByText('New Game'));
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+});
